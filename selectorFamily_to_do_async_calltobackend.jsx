13e6208f15dc1263fad255497e87d9e84bd4b704
Vishal-harne <123869546+Vishal-harne@users.noreply.github.com>
2024-10-28 17:37:40 +0530

    Create selectorFamily_to_do_async_calltobackend.jsx

---
+++ selectorFamily_to_do_async_calltobackend.jsx
@@ -0,0 +1,45 @@
+atom.js
+import { atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
+
+export const todosAtomFamily = atomFamily({
+  key: 'todosAtomFamily',
+  default: selectorFamily({
+    key: "todoSelectorFamily",
+    get: (id) => async ({get}) => {
+      const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+      return res.data.todo;
+    },
+  })
+});
+
+
+
+
+jsx file
+
+
+import './App.css'
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { todosAtomFamily } from './atoms';
+
+function App() {
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <Todo id={2} />
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+  return (
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  )
+}
+
+export default App
